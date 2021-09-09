7d66b56a019025540ca6b1d911dc3cf564360c3a
Jake Overall <joverall22@gmail.com>
2021-09-09 16:31:16 -0600

    Update Song.js

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -7,7 +7,7 @@ export default class Song {
     this.album = data.collectionName || data.album;
     this.price = data.trackPrice || data.price;
     this.preview = data.previewUrl || data.preview;
-    this.id = data.trackId || data.id;
+    this.id = data.trackId?.toString() || data.id;
   }
 
   get Template() {
